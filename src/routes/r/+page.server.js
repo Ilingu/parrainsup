ebf5319d980fd306b0bbc836d7553e82b6873592
Ilingu <ilingu@duck.com>
2024-08-29 14:23:57 +0200

    fix /r/, works again

--- src/routes/r/+page.server.js
+++ src/routes/r/+page.server.js
@@ -1,7 +1,8 @@
 import { getData, fromArray } from '$lib/parrain.js';
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ fetch, params }) {
 	const parrains = (await getData()).map(fromArray);
 	const nom = parrains[Math.floor(Math.random() * parrains.length)].nom;
-	return { nom };
+	throw redirect(302, '/p/' + nom);
 }
--- src/routes/r/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	export let data;
-
-	if (typeof window !== 'undefined') onMount(async () => goto('/p/' + data.nom));
-</script>
