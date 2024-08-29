1f9c8aa5daca67753405cf89bfaf3cfbfdcb0012
Ilingu <ilingu@duck.com>
2024-08-29 14:31:31 +0200

    oppsie 302!=307 🤭

--- src/routes/r/+page.server.js
+++ src/routes/r/+page.server.js
@@ -4,5 +4,5 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ fetch, params }) {
 	const parrains = (await getData()).map(fromArray);
 	const nom = parrains[Math.floor(Math.random() * parrains.length)].nom;
-	throw redirect(302, '/p/' + nom);
+	throw redirect(307, '/p/' + nom);
 }
